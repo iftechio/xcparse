df41acd47de5e104426ab1df2d93bdd0e5c058eb
mcfans <mcfans@icloud.com>
2020-09-01 19:42:09 +0800

    fix: use swiftPM lib directly

--- Package.swift
+++ Package.swift
@@ -25,10 +25,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "xcparse",
-            dependencies: [ "XCParseCore", "SPMUtility" ]),
+            dependencies: [ "XCParseCore", "SwiftPM" ]),
         .target(
             name: "XCParseCore",
-            dependencies: [ "SPMUtility" ]),
+            dependencies: [ "SwiftPM" ]),
         .testTarget(
             name: "xcparseTests",
             dependencies: ["xcparse"]),
